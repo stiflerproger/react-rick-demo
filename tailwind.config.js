4777207f4f53dfd30a5a6dce105a5548f1b9cf98
stiflerproger <roman_leon1995@outlook.com>
2024-12-14 14:06:21 +0500

    feat: tailwind setting

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",       // Для Vite
+    "./src/**/*.{js,ts,jsx,tsx}", // Все файлы с React компонентами
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
